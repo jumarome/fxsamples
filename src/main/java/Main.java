1a6a0acaae119efb4226fc1d0938fb046efc3217
Juan <jmanuelromero93@gmail.com>
2017-05-29 00:16:23 -0500

    Configuracion Inicial de los ejemplos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        this.window = primaryStage;
+        window.setTitle("Samples-Topic");
+        window.show();
     }
 }
