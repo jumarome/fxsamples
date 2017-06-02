390df1121d52ebcba4350bce1f70278a0971b016
Juan Romero Santamaría <jmanuelromero93@gmail.com>
2017-06-02 11:29:47 -0500

    Agregamos un atributo de tipo scene a la clase principal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     private Stage window;
+    private Scene scene;
 
     public static void main(String[] args) {
         launch(args);
