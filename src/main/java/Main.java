3cadd438bc8af143c18b28275a68c590d7fd48eb
Juan Romero Santamaría <jmanuelromero93@gmail.com>
2017-06-02 17:52:15 -0500

    Se permite agregar un producto a la tabla

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +20,7 @@ public class Main extends Application {
     private Scene scene;
     private Button button;
     private TableView<Product> table;
+    private TextField nameInput,priceInput,quantityInput;
 
     public static void main(String[] args) {
         launch(args);
@@ -42,16 +47,51 @@ public class Main extends Application {
         quantityColumn.setMinWidth(50);
         quantityColumn.setCellValueFactory(new PropertyValueFactory<Product, Integer>("quantity"));
 
+        //Name input
+        nameInput = new TextField();
+        nameInput.setPromptText("Name");
+        nameInput.setMinWidth(100);
+
+        //Price input
+        priceInput = new TextField();
+        priceInput.setPromptText("Price");
+
+
+        //Quantity input
+        quantityInput = new TextField();
+        quantityInput.setPromptText("Quantity");
+
+        Button addButton = new Button("Add");
+        addButton.setOnAction(e->addButtonClicked());
+        Button deleteButton = new Button("Delete");
+
+        HBox hBox = new HBox(10);
+        hBox.setPadding(new Insets(10,10,10,10));
+        hBox.getChildren().addAll(nameInput,priceInput,quantityInput,addButton,deleteButton);
+        //solamente para ver el espaciado
+        hBox.setBorder(new Border(new BorderStroke(Color.DARKGRAY, BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
+
         table = new TableView<>();
         table.setItems(getProducts());
         table.getColumns().addAll(nameColumn,priceColumn,quantityColumn);
         button = new Button("Click me");
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(table);
+        layout.getChildren().addAll(table,hBox);
         scene = new Scene(layout);
         window.setScene(scene);
         window.show();
+    }
+
+    private void addButtonClicked() {
+        Product product = new Product();
+        product.setName(nameInput.getText());
+        product.setPrice(Double.valueOf(priceInput.getText()));
+        product.setQuantity(Integer.valueOf(quantityInput.getText()));
+        table.getItems().add(product);
+        nameInput.clear();
+        priceInput.clear();
+        quantityInput.clear();
     }
 
     public ObservableList<Product> getProducts(){
