cdfc152f2c924320d94c6bc7e306d9122823825b
Juan Romero Santamaría <jmanuelromero93@gmail.com>
2017-06-02 12:24:26 -0500

    Definiendo estructura base de los ejemplos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     private Stage window;
     private Scene scene;
+    private Button button;
 
     public static void main(String[] args) {
         launch(args);
@@ -14,6 +18,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         this.window = primaryStage;
         window.setTitle("Samples-Topic");
+        button = new Button("Click me");
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20,20,20,20));
+        layout.getChildren().addAll(button);
+        scene = new Scene(layout,300,250);
+        window.setScene(scene);
         window.show();
     }
 }
