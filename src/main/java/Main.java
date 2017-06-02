544a8bf4ff825c74358f93b4f251299ee9e93c98
Juan Romero Santamaría <jmanuelromero93@gmail.com>
2017-06-02 13:20:29 -0500

    comboBox sample

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -10,6 +11,7 @@ public class Main extends Application {
     private Stage window;
     private Scene scene;
     private Button button;
+    private ComboBox<String> comboBox;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,12 +20,25 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         this.window = primaryStage;
         window.setTitle("Samples-Topic");
-        button = new Button("Click me");
+        button = new Button("Submit");
+        comboBox = new ComboBox<>();
+        comboBox.getItems().addAll(
+                "Volver al futuro",
+                "Harry Potter",
+                "Spiderman"
+        );
+        // A diferencia del ChoiceBox el promtText puede tener cualquier string
+        comboBox.setPromptText("Cual es tu pelicula favorita?");
+        comboBox.setOnAction(e->printMovie());
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(button);
+        layout.getChildren().addAll(comboBox,button);
         scene = new Scene(layout,300,250);
         window.setScene(scene);
         window.show();
+    }
+
+    private void printMovie() {
+        System.out.println("Has seleccionado: "+comboBox.getValue());
     }
 }
